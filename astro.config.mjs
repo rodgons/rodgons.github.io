e90859c65dd6bc648a75c6843d381676a0236343
Rodrigo Gonçalves da Silva <rodrigogoncalveess@gmail.com>
2025-09-27 00:55:12 -0700

    fix: add base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-//  site: "https://rodgons.github.io",
-//  base: "rodgons.github.io",
+  site: "https://rodgons.github.io",
+  base: "rodgons.github.io",
 
   vite: {
     plugins: [tailwindcss()],
